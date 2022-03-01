a0dbbf80ee7fbcbeb178be7920e055f8f2b79bef
Fabián Álvarez <fa.alvarez@uc.cl>
2022-02-28 23:16:36 -0300

    Fix regular expressions

--- 3_cleaning1.r
+++ 3_cleaning1.r
@@ -1,28 +1,40 @@
 source("1_libraries.r")
 source("2_source.r")
 
+
 # Speech 2020 (management 2019) Removing regular expressions --------------
 
-write_file(speech_2020_modif, "speech_2020_modif.txt", FALSE)
+# write_file(speech_2020_modif2, "speech_2020_modif2.txt", FALSE)
 
-speech_2020_modif <- speech_2020 %>%
-  str_remove_all("\"") %>% # Remove "\"" 
+# str_replace_all(a,
+#                 "",
+#                 "")
+
+# diffObj(speech_2020_modif1, speech_2020_modif2, mode="sidebyside")
+# diffChr(speech_2020_modif1, speech_2020_modif2, mode="sidebyside")
+
+
+speech_2020_modif2 <- speech_2020 %>%
   str_replace_all("\n", " ") %>% # Replace "\n" by space
+  str_remove_all("“") %>% str_remove_all("”") %>% # Remove "" 
   str_replace_all("MINJU", "MINJUDDHH") %>% # Ministerio de Justicia y DDHH
   str_replace_all("MINJUDDHH-DDHH", "MINJUDDHH") %>% 
-  str_remove_all("\\s+•\\s+") %>% # Bullets
+  str_replace_all("\\s+•\\s+", " ") %>% # Bullets
+  str_replace_all("\\s\\S+\\.-\\s", " ") %>% 
+  str_replace_all("\\s\\d-\\s", " ") %>% # Numbering
+  str_replace_all("\\s\\d\\.\\s", " ") %>% 
+  str_replace_all("\\s\\d\\d\\.\\s", " ") %>% 
+  str_replace_all("\\s\\d\\.\\d\\s", " ") %>% 
+  str_replace_all("\\s+[abcde]+\\)+\\s", " ") %>% 
+  str_replace_all("\\s[abcde]\\.\\d\\)", " ") %>% 
   str_remove_all("http\\S*") %>% # urls
   str_remove_all("www.\\S*") %>% # Remove web pages
-  str_replace_all("\\s+\\-+\\s", " ") %>% # Bullets
-  str_remove_all("\\d+-\\s") %>% # Numbering
-  str_replace_all("\\s+\\d+\\.+\\s", " ") %>% # Replace 1. by space
-  str_replace_all("\\d\\.", " ") %>% # Replace 1.2. by space
-  str_remove_all("\\s+[abcde]+\\)+\\s") %>% # Remove type of numbering a)  
-  str_replace_all("\\S+\\.+\\-", " ") %>% # Replace something like V.- by space 
+  
+
   str_remove_all("Twitter.+gendarmeriacl") %>%  # Remove social networks
   str_remove_all("N° de internos heridos.+\\(S\\.I\\.G\\)") %>% # Remove Tables
   str_remove_all("A continuación, se expone un desglose.+Fuente: Departamento de Infraestructura") %>% # Remove Tables 
-  str_remove_all("MATRICULADOS EN EDUCACIÓN SUPERIOR DICIEMBRE 2019.+Total.+163.+Fuente: Departamento Sistema Cerrado") %>% # Remove Tables 
+  str_remove_all("MATRICULADOS EN EDUCACIÓN SUPERIOR DICIEMBRE 2019.+Total\\s+163\\s+Fuente: Departamento Sistema Cerrado") %>% # Remove Tables 
   str_remove_all("\\s+Tabla 1. Privados de Libertad Inscritos para dar PSU.+Fuente: Departamento Sistema Cerrado") %>% # Remove Tables 
   str_remove_all("\\s+Tabla 2. Resultados PSU 2019 de Privados de Libertad, por región:.+Fuente: Departamento Sistema Cerrado") %>% # Remove Tables 
   str_remove_all("\\s+Intervención:.+Fuente: Departamento Sistema Cerrado") %>% # Remove Tables 
