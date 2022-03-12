d26bdca7bcc3be49256b676adb947bbb9143d680
Fabián Álvarez <fa.alvarez@uc.cl>
2022-03-12 01:32:34 -0300

    Improve removing regular expressions

--- 3_cleaning3.r
+++ 3_cleaning3.r
@@ -3,21 +3,33 @@
 
 # Speech 2018 (management 2017) Removing regular expressions --------------
 
+# diffObj(speech_2018_modif1, speech_2018_modif2, mode="sidebyside")
+# diffChr(speech_2018_modif1, speech_2018_modif2, mode="sidebyside")
+
 speech_2018 <- speech_2018 %>% 
-  str_replace_all("\r\n", " ") %>%
-  str_replace_all("MINJU-DDHH", "MINJUDDHH") %>% # Ministerio de Justicia y DDHH
-  str_replace_all("MINJU,", "MINJUDDHH") %>% 
-  str_replace_all("(MINJU)", "MINJUDDHH") %>% 
-  str_remove_all("\\s+•\\s+") %>% # Bullets
-  str_remove_all("\\s+\\-+\\s") %>% # Bullets  
-  str_remove_all("\\s+Allanamientos.+7+\\.+854") %>%   
-  str_remove_all("\\s+Establecimientos Penitenciarios.+43") %>% 
-  str_remove_all("\\s+Para el año 2018 se desarrollarán.+305") %>% 
-  str_remove_all("\\s+N° de horas.+\\(+LA ARAUCANÍA+\\)") %>%   
-  str_remove_all("\\s+Los beneficiados de los programas.+DIPLOMADO GERENCIA PUBLICA+\\s+\\d") %>%     
-  str_replace_all("\\s+\\d+\\.+\\s", " ") %>%  # 1. 
-  str_replace_all("I+\\.+\\s+AVANCES", "AVANCE") %>%
-  str_replace_all("\\s+II+\\.+\\s", " ") %>%  
+  str_replace_all("\n", " ") %>% # Replace "\n" by space
+  str_remove_all("\\s+Allanamientos\\s+.+854") %>% # Remove Tables
+  str_remove_all("\\s+PROGRAMAS\\s+RE.+LACTANTES") %>% 
+  str_remove_all("Distribución Regional.+NACIONAL\\s+43") %>% 
+  str_remove_all("siguiente detalle.+Total\\s+305") %>% 
+  str_remove_all("Aspirante Oficiales P.+327 Hombres\\)") %>% 
+  str_remove_all("\\s+N° de horas.+ARAUCANÍA\\)") %>% 
+  str_remove_all("AÑO NOMBRE.+PUBLICA 30")
+
+
+# Speech 2018 (management 2017) - Additional corrections ------------------
+
+# diffChr(speech_2018_modif1, speech_2018_modif2, mode="sidebyside")
+
+speech_2018 <- speech_2018 %>% 
+  str_remove_all("N°") %>% 
+  str_replace_all("Departamento del Sistema Cerrado\\.", " ") %>% 
+  str_replace_all("Departamento de Recursos Humanos", " ") %>% 
+  str_replace_all("multidisclinarios", "multidisciplinarios") %>% 
+  str_replace_all("asociadas:Implementación", "asociadas Implementación") %>% 
+  str_replace_all("pesos\\.Reposición", "pesos Reposición") %>% 
+  str_replace_all("incendios:Programa", "incendios Programa") %>% 
+  str_replace_all("Coyhaique\\.Mantención", "Coyhaique Mantención") %>% 
   stripWhitespace() 
 
 
